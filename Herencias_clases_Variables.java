dffcfcd9cad13a7224d44c9ea6fade7bf449d385
Kevin Sanchez <100317309+kevsanz12@users.noreply.github.com>
2022-06-29 20:28:18 +0200

    Add files via upload

---
+++ Herencias_clases_Variables.java
@@ -0,0 +1,31 @@
+public class Herencias_clases_Variables {
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.laEdad = 25;
+        cliente.elTelefono = 1744309853;
+        cliente.nombre = "kevin";
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.elTelefono =1744309853;
+        trabajador.laEdad = 25;
+        trabajador.nombre = "kevin";
+    }
+    class Persona {
+        int laEdad;
+        String nombre;
+        int elTelefono;
+    public Persona(){
+        System.out.println("datos de las personas");
+    }
+    }
+    class Cliente extends Persona{
+        int credito;
+    }
+
+    class Trabajador extends Persona{
+    int salario;
+    }
+
+}
+
